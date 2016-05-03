e4d235ef7ad205fe6af078a8753d7367522b79a9
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-05-03 18:06:02 +0200

    Update tough-cookie and add a test with a request.

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,34 @@
 import * as test from 'blue-tape';
+import * as Bluebird from "bluebird";
 
 import * as request from 'request';
 
-test('request', (t) => {
+test('request-general', (t) => {
   t.plan(1);
   t.equal(typeof request, 'function');
+});
+
+
+test('request-get', (t) => {
+  const URL = "https://api.typings.org/entries/npm/request";
+
+  let options: request.CoreOptions & request.UriOptions = {
+    headers: {},
+    timeout: 60000,
+    qs: {},
+    uri: URL,
+    method: "GET",
+    jar: null,
+    gzip: true
+  };
+
+  return Bluebird.fromCallback((cb) => {
+    request(options, (error, response, body) => {
+      if (error) {
+        return cb(error);
+      }
+      t.equal(typeof response.statusCode, "number");
+      return cb(null, null);
+    });
+  });
 });
